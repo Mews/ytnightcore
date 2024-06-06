723a00817edb62f71674324181e6321186988d4a
Mews <ar754456@gmail.com>
2024-06-06 21:59:17 +0100

    Line returned from popline isn't stripped anymore

--- src/consoleio.py
+++ src/consoleio.py
@@ -3,6 +3,8 @@ from io import StringIO
 class ConsoleIO(StringIO):
     """
     This is a StringIO that calls the function passed as callback whenever its content changes
+
+    It also has a method to read a line and remove it which is popline
     """
 
     def __init__(self, callback, *args, **kwargs):
@@ -24,3 +26,21 @@ class ConsoleIO(StringIO):
     def seek(self, pos, mode=0):
         super().seek(pos, mode)
         self.callback()
+    
+    def popline(self):
+        #Disable the callback while running the function to avoid infinite recursion
+        og_callback = self.callback
+        self.callback = lambda: None
+
+        self.seek(0) # Set the position to the beggining of the IO to read the first line
+        line = self.readline()
+
+        remaining_content = self.getvalue()[len(line):]
+
+        self.truncate(0) # Clear the StringIO
+        self.seek(0) # Set the position to the beggining of the IO
+        self.write(remaining_content) # Write the content without the line
+
+        self.callback = og_callback
+
+        return line
