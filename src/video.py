cead9d83e88414ac503037abd59088a9a472e676
Mews <ar754456@gmail.com>
2024-06-07 11:22:39 +0100

    Video thumbnail image now has duration indicator at the bottom right corner

--- src/video.py
+++ src/video.py
@@ -1,4 +1,6 @@
 from thumbnail import Thumbnail
+from PIL import ImageDraw, ImageFont
+
 class Video:
     def __init__(self, raw_data):
         self.raw_data = raw_data
@@ -41,7 +43,40 @@ class Video:
         self.thumbnail_image = None
     
     def get_thumbnail_image(self, edge_radius=0):
-        img = self.thumbnail.get_image(edge_radius=edge_radius)
+        image_width = 282
+        image_height = 159
 
-        self.thumbnail_image = img
-        return img
+        corner_radius = 5
+
+        text = self.duration
+
+        image = self.thumbnail.get_image(edge_radius=edge_radius).resize((image_width, image_height))
+
+        # Draw duration at bottom right corner
+        draw = ImageDraw.Draw(image)
+
+        font = ImageFont.load_default()
+
+        # Calculate text size
+        text_bbox = draw.textbbox((0,0), text)
+        text_width = text_bbox[2] - text_bbox[0]
+        text_height  = text_bbox[3] - text_bbox[1]
+
+        # Calculate rectangle height (some padding for the text)
+        padding = 7
+        rect_height = text_height + 2 * padding
+
+        # Draw the rounded rectangle at the bottom right corner
+        image_width, image_height = image.size
+        rect_position = (image_width - text_width - padding, image_height - rect_height, image_width, image_height)
+        draw.rounded_rectangle(rect_position, fill="black", radius=corner_radius)
+
+        # Calculate text position
+        text_x = image_width - text_width - padding / 2
+        text_y = image_height - rect_height + padding / 2
+
+        # Draw the text on top of the rectangle
+        draw.text((text_x, text_y), text, font=font, fill="white")
+
+        self.thumbnail_image = image
+        return image
