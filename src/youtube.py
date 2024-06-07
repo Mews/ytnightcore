5651281d842693a5594fd1941ab98ce94e6da114
Mews <ar754456@gmail.com>
2024-06-07 19:42:05 +0100

    Added option for a progress hook in download_yt_mp3

--- src/youtube.py
+++ src/youtube.py
@@ -17,7 +17,7 @@ def search(query, limit=1) -> List[Video]:
     return videos
 
 
-def download_yt_mp3(url:str, output_path:str):
+def download_yt_mp3(url:str, output_path:str, progress_hook=lambda _:None):
     ydl_options = {
     "format": "bestaudio",
     "outtmpl": output_path,
@@ -25,7 +25,8 @@ def download_yt_mp3(url:str, output_path:str):
         "key": "FFmpegExtractAudio",
         "preferredcodec": "mp3",
         "preferredquality": "192",
-    }]
+    }],
+    "progress_hooks": [progress_hook],
     }
 
     with YoutubeDL(ydl_options) as ydl:
