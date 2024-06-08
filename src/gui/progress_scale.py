bb03c42f284299362ff060cd076e1e7c54a20e96
Mews <ar754456@gmail.com>
2024-06-08 21:59:01 +0100

    Added extra options to the progress scale

--- src/gui/progress_scale.py
+++ src/gui/progress_scale.py
@@ -1,19 +1,19 @@
 import ttkbootstrap as ttk
 
-class CustomProgressBar(ttk.Canvas):
-    def __init__(self, master=None, width=300, height=30, **kwargs):
+class ProgressScale(ttk.Canvas):
+    def __init__(self, master=None, width=300, height=30, bgcolor="lightgray", fgcolor="lightblue", handle_radius=15, **kwargs):
         super().__init__(master, width=width, height=height, **kwargs)
         self.width = width
         self.height = height
         self.progress = 0  # Progress as a percentage (0 to 100)
-        self.trail_color = 'lightgray'
-        self.progress_color = 'blue'
-        self.handle_color = 'darkblue'
+        self.trail_color = bgcolor
+        self.progress_color = fgcolor
+        self.handle_color = fgcolor
         self.trail = self.create_rectangle(0, self.height * 0.65, width, height * 0.35, fill=self.trail_color)
         self.rect = self.create_rectangle(0, self.height * 0.65, 0, height * 0.35, fill=self.progress_color)
-        self.handle_radius = height // 2
+        self.handle_radius = handle_radius
         self.handle = self.create_oval(
-            0, 0, self.handle_radius * 2, self.handle_radius * 2, fill=self.handle_color
+            0, 0, handle_radius * 2, handle_radius * 2, fill=self.handle_color
         )
         self.bind("<Button-1>", self.start_drag)
         self.bind("<B1-Motion>", self.on_drag)
@@ -39,10 +39,9 @@ class CustomProgressBar(ttk.Canvas):
         self.coords(self.trail, 0, self.height * 0.65, self.width, self.height * 0.35)
         self.coords(self.rect, 0, self.height * 0.65, (self.progress / 100) * self.width, self.height * 0.35)
         handle_center = (self.progress / 100) * self.width
+        handle_radius = self.handle_radius
         self.coords(
             self.handle,
-            handle_center - self.handle_radius,
-            0,
-            handle_center + self.handle_radius,
-            self.height,
+            handle_center - handle_radius, (self.height / 2) - handle_radius,
+            handle_center + handle_radius, (self.height / 2) + handle_radius,
         )
